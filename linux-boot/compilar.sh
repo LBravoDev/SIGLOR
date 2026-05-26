ed8eccdc877fe324d3c86026022e80d2a234e8f6
LBravoDev <bravolautaro2007ws@gmail.com>
2026-05-26 20:22:03 -0300

    Portabilidad inicial para Linux agregada
    Se agrego un script de compilacion especifico para Linux debido que el que ya se encuentra en el proyecto es unicamente para Windows. Además se incluyó la versión apta para Linux del archivo libraylib.a para que corriera exitosamente.

---
+++ linux-boot/compilar.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Pararse automáticamente en la carpeta linux-boot (donde está este script)
+cd "$(dirname "$0")"
+
+echo "================================================="
+echo " Compilando proyecto para Linux (Port Autónomo) "
+echo "================================================="
+
+
+# Compilación:
+# -I y -L apuntan a las carpetas subiendo un nivel (../)
+# Pero el -L principal para Raylib ahora busca EN ESTA CARPETA (.) el archivo libraylib.a de Linux
+g++ ../fuentes/main.cpp ../fuentes/Interfaz.cpp ../fuentes/Grafo.cpp \
+    -o ../ejecutables/programa_linux \
+    -I ../Raylib -I ../cabeceras \
+    -L . \
+    -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 \
+    -Wno-enum-compare
+
+if [ $? -eq 0 ]; then
+    echo "¡Éxito! Abriendo programa..."
+    ../ejecutables/programa_linux
+else
+    echo ""
+    echo "[ERROR] Hubo un problema en la compilación. Revisa el código arriba."
+    read -p "Presiona Enter para salir..."
+fi
